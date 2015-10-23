352c67e36b8cad16c8aa3fd9b5536a1ed0694765
Florian Eckerstorfer <florian@eckerstorfer.co>
2015-10-23 14:13:06 +0200

    Add EntityWriter for Doctrine ORM
    Fixes #1

---
+++ src/ORM/EntityWriter.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Plum\PlumDoctrine\ORM;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Plum\Plum\Writer\WriterInterface;
+
+/**
+ * EntityWriter
+ *
+ * @package   Plum\PlumDoctrine\ORM
+ * @author    Florian Eckerstorfer
+ * @copyright 2015 Florian Eckerstorfer
+ */
+class EntityWriter implements WriterInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * @var array
+     */
+    private $options = [
+        'flushInterval' => null,
+    ];
+
+    /**
+     * @var int
+     */
+    private $persistCount = 0;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     * @param array                  $options
+     *
+     * @codeCoverageIgnore
+     */
+    public function __construct(EntityManagerInterface $entityManager, array $options = [])
+    {
+        $this->entityManager = $entityManager;
+        $this->options       = array_merge($this->options, $options);
+    }
+
+    /**
+     * Write the given item.
+     *
+     * @param mixed $item
+     *
+     * @return void
+     */
+    public function writeItem($item)
+    {
+        $this->entityManager->persist($item);
+        $this->persistCount++;
+        if ($this->options['flushInterval'] && $this->shouldFlush()) {
+            $this->finish();
+        }
+    }
+
+    /**
+     * Prepare the writer.
+     *
+     * @return void
+     */
+    public function prepare()
+    {
+        $this->persistCount = 0;
+    }
+
+    /**
+     * Finish the writer.
+     *
+     * @return void
+     */
+    public function finish()
+    {
+        $this->persistCount > 0 && $this->entityManager->flush();
+    }
+
+    /**
+     * @return bool
+     */
+    protected function shouldFlush()
+    {
+        return ($this->persistCount % $this->options['flushInterval']) === 0;
+    }
+}
---
+++ tests/ORM/EntityWriterTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Plum\PlumDoctrine\ORM;
+
+use Mockery;
+use PHPUnit_Framework_TestCase;
+use ReflectionClass;
+use stdClass;
+
+/**
+ * EntityWriterTest
+ *
+ * @package   Plum\PlumDoctrine\ORM
+ * @author    Florian Eckerstorfer
+ * @copyright 2015 Florian Eckerstorfer
+ * @group     unit
+ */
+class EntityWriterTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Doctrine\ORM\EntityManagerInterface|\Mockery\MockInterface
+     */
+    private $entityManager;
+
+    public function setUp()
+    {
+        $this->entityManager = Mockery::mock('\Doctrine\ORM\EntityManagerInterface');
+    }
+
+    /**
+     * @test
+     * @covers Plum\PlumDoctrine\ORM\EntityWriter::prepare()
+     */
+    public function prepareShouldResetPersistCount()
+    {
+        $this->entityManager->shouldReceive('persist');
+
+        $writer = new EntityWriter($this->entityManager);
+        $writer->writeItem(new stdClass());
+        $writer->prepare();
+
+        $this->assertSame(0, $this->getReflectionProperty($writer, 'persistCount'));
+    }
+
+    /**
+     * @test
+     * @covers Plum\PlumDoctrine\ORM\EntityWriter::writeItem()
+     */
+    public function writeItemPersistsItemInEntityManager()
+    {
+        $entity = new stdClass();
+
+        $this->entityManager->shouldReceive('persist')->with($entity)->once();
+
+        $writer = new EntityWriter($this->entityManager);
+        $writer->writeItem($entity);
+    }
+
+    /**
+     * @test
+     * @covers Plum\PlumDoctrine\ORM\EntityWriter::writeItem()
+     * @covers Plum\PlumDoctrine\ORM\EntityWriter::finish()
+     * @covers Plum\PlumDoctrine\ORM\EntityWriter::shouldFlush()
+     */
+    public function writeItemShouldFlushIfFlushIntervalIsSet()
+    {
+        $entity = new stdClass();
+
+        $this->entityManager->shouldReceive('persist')->with($entity)->times(2);
+        $this->entityManager->shouldReceive('flush')->once();
+
+        $writer = new EntityWriter($this->entityManager, ['flushInterval' => 2]);
+        $writer->writeItem($entity);
+        $writer->writeItem($entity);
+    }
+
+    /**
+     * @test
+     * @covers Plum\PlumDoctrine\ORM\EntityWriter::finish()
+     */
+    public function finishFlushesTransactions()
+    {
+        $this->entityManager->shouldReceive('persist');
+        $this->entityManager->shouldReceive('flush')->once();
+
+        $writer = new EntityWriter($this->entityManager);
+        $writer->writeItem(new stdClass());
+        $writer->finish();
+    }
+
+    /**
+     * @test
+     * @covers Plum\PlumDoctrine\ORM\EntityWriter::finish()
+     */
+    public function finishShouldOnlyFlushIfItemIsWritten()
+    {
+        $this->entityManager->shouldReceive('flush')->never();
+
+        $writer = new EntityWriter($this->entityManager);
+        $writer->finish();
+    }
+
+    /**
+     * @param object $object
+     * @param string $property
+     *
+     * @return mixed
+     */
+    protected function getReflectionProperty($object, $property)
+    {
+        $reflectionClass    = new ReflectionClass(get_class($object));
+        $reflectionProperty = $reflectionClass->getProperty($property);
+        $reflectionProperty->setAccessible(true);
+
+        return $reflectionProperty->getValue($object);
+    }
+}
