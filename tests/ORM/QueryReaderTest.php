0d6e1eb6b8333468ee256e3ac7efc279fcefd632
Florian Eckerstorfer <florian@eckerstorfer.co>
2015-10-23 14:51:41 +0200

    Add QueryReader for Doctrine ORM
    Fixes #4

---
+++ tests/ORM/QueryReaderTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Plum\PlumDoctrine\ORM;
+
+use Doctrine\ORM\AbstractQuery;
+use Mockery;
+use PHPUnit_Framework_TestCase;
+use stdClass;
+
+/**
+ * QueryReaderTest
+ *
+ * @package   Plum\PlumDoctrine\ORM
+ * @author    Florian Eckerstorfer
+ * @copyright 2015 Florian Eckerstorfer
+ * @group     unit
+ */
+class QueryReaderTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Doctrine\ORM\AbstractQuery|\Mockery\MockInterface
+     */
+    private $query;
+
+    public function setUp()
+    {
+        $this->query = Mockery::mock('\Doctrine\ORM\AbstractQuery');
+    }
+
+    /**
+     * @test
+     * @covers Plum\PlumDoctrine\ORM\QueryReader::getIterator()
+     */
+    public function getIteratorShouldReturnIteratorForQuery()
+    {
+        $result = new stdClass();
+        $this->query->shouldReceive('getResult')->once()->andReturn([$result]);
+
+        $reader   = new QueryReader($this->query);
+        $iterator = $reader->getIterator();
+
+        $this->assertInstanceOf('\Iterator', $iterator);
+        foreach ($iterator as $entity) {
+            $this->assertEquals($result, $entity);
+        }
+    }
+
+    /**
+     * @test
+     * @covers Plum\PlumDoctrine\ORM\QueryReader::getIterator()
+     */
+    public function getIteratorShouldPassHydrationModeToDoctrine()
+    {
+        $result = new stdClass();
+        $this->query->shouldReceive('getResult')->with(AbstractQuery::HYDRATE_ARRAY)->once()->andReturn([$result]);
+
+        $reader   = new QueryReader($this->query, ['hydrationMode' => AbstractQuery::HYDRATE_ARRAY]);
+        $iterator = $reader->getIterator();
+
+        $this->assertInstanceOf('\Iterator', $iterator);
+        foreach ($iterator as $entity) {
+            $this->assertEquals($result, $entity);
+        }
+    }
+
+    /**
+     * @test
+     * @covers Plum\PlumDoctrine\ORM\QueryReader::getIterator()
+     */
+    public function getIteratorShouldCallGetResultOnlyOnce()
+    {
+        $this->query->shouldReceive('getResult')->once()->andReturn([]);
+
+        $reader = new QueryReader($this->query);
+        $reader->getIterator();
+        $reader->getIterator();
+    }
+
+    /**
+     * @test
+     * @covers Plum\PlumDoctrine\ORM\QueryReader::count()
+     */
+    public function countShouldReturnNumberOfResults()
+    {
+        $result = new stdClass();
+        $this->query->shouldReceive('getResult')->andReturn([$result, $result]);
+
+        $reader = new QueryReader($this->query);
+
+        $this->assertEquals(2, $reader->count());
+    }
+}
